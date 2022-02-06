6604842051244c61c275e8ee735d7fd1e45aa1dd
Nam <64239237+namisama269@users.noreply.github.com>
2022-02-06 20:02:24 +1100

    Time: 73 ms (22.23%), Space: 12.3 MB (59.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -18,7 +18,6 @@ public:
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == '1') {
-                    cout << i << " " << j << '\n';
                     floodFill(grid, i, j, m, n);
                     ++ans;
                 }
