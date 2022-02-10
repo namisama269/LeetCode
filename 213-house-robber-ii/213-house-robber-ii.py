2261a70a69580f6d8bb955c9f9ef96a6548dd7ec
Nam <64239237+namisama269@users.noreply.github.com>
2022-02-10 19:42:13 +1100

    Time: 44 ms (46.16%), Space: 13.9 MB (83.84%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return max(nums)
+        return max(solve(nums[1:]), solve(nums[:n-1]))
+        
+def solve(nums):
+    n = len(nums)
+    dp = [0] * n
+
+    dp[0] = nums[0]
+    if n > 1:
+        dp[1] = max(dp[0], nums[1])
+
+    for i in range(2, n):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+    return dp[n-1]
