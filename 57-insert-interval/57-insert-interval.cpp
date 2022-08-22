d6ccbd5d2471d599048b5b8d60b45da3e61fcab8
Nam <64239237+namisama269@users.noreply.github.com>
2022-08-22 20:18:04 +1000

    Time: 17 ms (84.15%), Space: 17 MB (75.36%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -5,12 +5,9 @@ public:
         vector<vector<int>> ans;
                 
         for (int i = 0; i < n; ++i) {
-            // no collision left
             if (intervals[i][1] < newInterval[0]) {
                 ans.push_back(intervals[i]);
-            }
-            // no collision right
-            else if (newInterval[1] < intervals[i][0]) {
+            } else if (newInterval[1] < intervals[i][0]) {
                 ans.push_back(newInterval);
                 ans.insert(ans.end(), intervals.begin() + i, intervals.end());
                 return ans;
@@ -18,7 +15,6 @@ public:
                 newInterval[0] = min(newInterval[0], intervals[i][0]);
                 newInterval[1] = max(newInterval[1], intervals[i][1]);
             }
-            
         }
         
         ans.push_back(newInterval);
